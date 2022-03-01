635b56713f2e44f3a0b52dda83b6fb627f752d50
SHAO NINGYU <87936912+shaoningyu1231@users.noreply.github.com>
2022-03-02 08:23:05 +0900

    Create 456. 132 Pattern.py
    This is a very tricky question for stack. Because when we are trying to traverse the list from beginning, we have to find a value between the max and min, and at the same time the index of mid should be bigger than the index of max and min. So we can go backwards, to traverse from the end. Because we can first get the third number and find the bigger and smaller one, which would be much easier to solve.

---
+++ 456. 132 Pattern.py
@@ -0,0 +1,12 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        third = -float("inf")
+        stack = []
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] < third:
+                return True
+            while stack and stack[-1] < nums[i]:
+                third = stack.pop()
+            stack.append(nums[i])
+        return False
+            
